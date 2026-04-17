56db4001cb0baabc1c4ae1ddcb98428f84441c8a
Kevin Carter <37614822+thisiswhom@users.noreply.github.com>
2026-04-17 15:49:26 +0000

    trying to brnach

--- run_exercises.py
+++ run_exercises.py
@@ -1,2 +1,3 @@
-my_portfolio = portfolio.data.create_portfolio("Retirement")
-portfolio.report.print_report(my_portfolio)
+if __name__ = "__main__":
+    my_portfolio = portfolio.data.create_portfolio("Retirement")
+    portfolio.report.print_report(my_portfolio)
